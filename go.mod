6aec809d352d510404763c08e696cc9dbe85ed99
bugracdnc <bugracaliskandinc@gmail.com>
2024-09-01 22:40:41 +0300

    Add: add, remove and clear commands

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module todoer
+
+go 1.23.0
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -8,17 +8,20 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/google/uuid"
 )
 
-// Todoer struct
+// TODOER STRUCT
 type Todoer struct {
-	Id          string `json:"id"`
-	Todo        string `json:"todo"`
-	UpdateDate  string `json:"updateDate"`
-	CreatedDate string `json:"createdDate"`
+	Id          uuid.UUID `json:"id"`
+	Todo        string    `json:"todo"`
+	UpdateDate  string    `json:"updateDate"`
+	CreatedDate string    `json:"createdDate"`
 }
 
 func (t *Todoer) getCreatedDate() (time.Time, error) {
@@ -34,56 +37,63 @@ func (t *Todoer) getCreatedDate() (time.Time, error) {
 }
 
 func buildTodoer(todo string) *Todoer {
-	return &Todoer{Id: "", UpdateDate: "", CreatedDate: "", Todo: todo}
+	return &Todoer{Id: uuid.UUID{}, UpdateDate: "", CreatedDate: "", Todo: todo}
 }
 
-//end Todoer struct
+var repo []Todoer
+
+//END TODOER STRUCT
 
 // HTTP PARAMETERS
+const serverUrl = "http://192.168.1.241"
 const serverPort = 8080
 const basePath = "/api/v1/todoer"
 
 //END HTTP PARAMETERS
 
 // REQUESTS
-func makeListRequest() {
-	requestURL := fmt.Sprintf("http://localhost:%d%s", serverPort, basePath)
+func makeListRequest() []Todoer {
+	requestURL := fmt.Sprintf("%s:%d%s", serverUrl, serverPort, basePath)
 	res, errGet := http.Get(requestURL)
 	if errGet != nil {
 		panic(errGet)
 	}
 
-	if res.StatusCode == http.StatusOK {
-		body, errReadBody := io.ReadAll(res.Body)
-		if errReadBody != nil {
-			panic(errReadBody)
-		}
-		var todoers []Todoer
-		errUnmarshal := json.Unmarshal(body, &todoers)
-		if errUnmarshal != nil {
-			panic(errUnmarshal)
-		}
-		for index, todoer := range todoers {
-			date, errDate := todoer.getCreatedDate()
-			dateString := fmt.Sprintf("%d:%d %02d/%02d", date.Hour(), date.Minute(), date.Day(), date.Month())
-			if errDate != nil {
-				fmt.Printf("%d) %s\n", index+1, todoer.Todo)
-			} else {
-				fmt.Printf("%d) %s - %s\n", index+1, todoer.Todo, dateString)
-			}
-		}
+	if res.StatusCode != http.StatusOK {
+		return []Todoer{}
 	}
+
+	body, errReadBody := io.ReadAll(res.Body)
+	if errReadBody != nil {
+		panic(errReadBody)
+	}
+	var todoers []Todoer
+	errUnmarshal := json.Unmarshal(body, &todoers)
+	if errUnmarshal != nil {
+		panic(errUnmarshal)
+	}
+	sort.Slice(todoers, func(i, j int) bool {
+		d1, err1 := todoers[i].getCreatedDate()
+		d2, err2 := todoers[j].getCreatedDate()
+		if err1 == nil && err2 == nil {
+			return d1.Before(d2)
+		}
+		if err1 != nil {
+			return false
+		}
+		return false
+	})
+	return todoers
 }
 
-func makeAddRequest(str string) (*Todoer, error) {
+func makeAddRequest(str string) (bool, string) {
 	todoer := buildTodoer(str)
-	requestURL := fmt.Sprintf("http://localhost:%d%s", serverPort, basePath)
+	requestURL := fmt.Sprintf("%s:%d%s", serverUrl, serverPort, basePath)
 
 	body, err := json.Marshal(todoer)
 
 	if err != nil {
-		fmt.Printf("error in json.Marshal: %s", err)
-		return nil, err
+		panic(err)
 	}
 
 	res, errPost := http.Post(requestURL, "application/json", bytes.NewBuffer(body))
@@ -92,32 +102,86 @@ func makeAddRequest(str string) (*Todoer, error) {
 		panic(errPost)
 	}
 
-	fmt.Printf("client: status code: %d\n", res.StatusCode)
 	if res.StatusCode == http.StatusCreated {
-		fmt.Printf("client: Location: %s\n", res.Header.Get("Location"))
+		return true, fmt.Sprintf("Added new to Location: %s%s\n", serverUrl, res.Header.Get("Location"))
+	}
+	return false, strconv.Itoa(res.StatusCode)
+}
+
+func makeDeleteRequest(uuid uuid.UUID) bool {
+	client := &http.Client{}
+	requestURL := fmt.Sprintf("%s:%d%s/%s", serverUrl, serverPort, basePath, uuid.String())
+	req, err := http.NewRequest("DELETE", requestURL, nil)
+	if err != nil {
+		panic(err)
 	}
 
-	return todoer, nil
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode == http.StatusNoContent {
+		return true
+	} else {
+		fmt.Printf("unhandled error: %d\n", res.StatusCode)
+		return false
+	}
 }
 
 //END REQUESTS
 
 // COMMANDS
 func list() {
-	makeListRequest()
-}
-func add(str string) {
-	_, err := makeAddRequest(str)
-
-	if err != nil {
-		return
+	repo = makeListRequest()
+	if len(repo) > 0 {
+		fmt.Printf("Current List: {%d items}\n", len(repo))
+		for index, todoer := range repo {
+			date, errDate := todoer.getCreatedDate()
+			dateString := fmt.Sprintf("%d:%d %02d/%02d", date.Hour(), date.Minute(), date.Day(), date.Month())
+			if errDate != nil {
+				fmt.Printf("%d) %s\n", index+1, todoer.Todo)
+			} else {
+				fmt.Printf("%d) %s - %s\n", index+1, todoer.Todo, dateString)
+			}
+		}
+		fmt.Println()
+	} else {
+		fmt.Println("Todoers is empty!")
 	}
 }
-func move(str string) {
-	fmt.Printf("move: %s", str)
+func add(str string) {
+	isAdded, result := makeAddRequest(str)
+	if isAdded {
+		fmt.Println(result)
+	} else {
+		fmt.Printf("error: status code %s", result)
+	}
 }
+
 func remove(str string) {
-	fmt.Printf("remove: %s", str)
+	if len(repo) == 0 {
+		repo = makeListRequest()
+	}
+
+	if str == "all" {
+		for _, todoer := range repo {
+			makeDeleteRequest(todoer.Id)
+		}
+		fmt.Printf("Cleared the list!")
+	} else {
+		index, err := strconv.Atoi(str)
+		if err != nil {
+			panic(err)
+		}
+		index--
+		if index <= len(repo) {
+			if makeDeleteRequest(repo[index].Id) {
+				fmt.Printf("Deleted todo: %s\n\n", repo[index].Todo)
+				list()
+			}
+		}
+	}
 }
 
 //END COMMANDS
@@ -133,8 +197,8 @@ func usage() {
 		"-h":     "returns usage information",
 		"--help": "returns usage information",
 		"add":    "adds new todo",
-		"move":   "moves given todo to new index",
-		"remove": "removes given todo"}
+		"remove": "removes given todo",
+		"clear":  "clears the todo list (same as 'todoer remove all')"}
 
 	//Print hardcoded usage example and commands title
 	fmt.Println("usage:\n\ttodoer <command> [arguments]")
@@ -160,7 +224,7 @@ func usage() {
 func main() {
 	args := os.Args[1:]
 
-	//fmt.Printf("debug:\tcommand=%s\n\n", args)
+	repo = makeListRequest()
 
 	if len(args) > 0 {
 		switch args[0] {
@@ -172,14 +236,12 @@ func main() {
 			if len(args) > 1 {
 				add(strings.Join(args[1:], " "))
 			}
-		case "move":
-			if len(args) > 1 {
-				move(strings.Join(args[1:], " "))
-			}
 		case "remove":
 			if len(args) > 1 {
 				remove(strings.Join(args[1:], " "))
 			}
+		case "clear":
+			remove("all")
 		default:
 			fmt.Println("error: not implemented yet")
 		}
